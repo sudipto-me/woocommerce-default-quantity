2e29fe120226092d63b61a042902843f314fcd9c
Sudipto Shakhari <sudipto.shakhari@selise.ch>
2022-11-15 11:40:31 +0600

    add npm file

---
+++ uninstall.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Default_Product_Quantity Uninstall
+ *
+ * Uninstalling Default_Product_Quantity deletes user roles, pages, tables, and options.
+ *
+ * @package WooCommerce_Default_Quantity\Uninstaller
+ * @version 1.0.0
+ */
+
+defined( 'WP_UNINSTALL_PLUGIN' ) || exit;
